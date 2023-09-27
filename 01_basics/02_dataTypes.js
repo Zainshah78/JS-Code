db444c24edfb9f1e5daa79d38056df1eec92f1ee
Zain <84030976+Zainshah78@users.noreply.github.com>
2023-09-27 05:00:47 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer versivion
+
+// alert("kello (3 + 3)"); // we are using node.js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("zain");
+
+let name = "zian"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// primitive
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
