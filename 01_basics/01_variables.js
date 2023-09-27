f53ebeb2b1ed90ca3fc2d2c58e8df7cf6f2e85e4
Zain <84030976+Zainshah78@users.noreply.github.com>
2023-09-27 04:58:52 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1769
+let accountEmail = "zain@google.com"
+var accountPassword = "ms-12345"
+accountCity = "Islamabad"
+let accountState;
+
+/* accountId = 2 
+console.log(accountId); */
+
+accountEmail ="syed@icom"
+accountPassword = "987u98jh"
+accountCity = "rawalpindi"
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope 
+ */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
