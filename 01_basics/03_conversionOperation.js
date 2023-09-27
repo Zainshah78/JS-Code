85bc9f34fa00ba6bd5be660d3a9dc1127a58e09c
Zain <84030976+Zainshah78@users.noreply.github.com>
2023-09-27 05:39:05 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+ let score = "88" 
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+//  console.log(typeof (valueInNumber));
+
+// "33" => 33
+// "88abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "zain" => true 
+
+let someNumber = 66
+let stringNumber = string(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
